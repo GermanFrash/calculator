06c7e3ac6d950cb1954dba479104044c0a7e04e2
GermanFrash <166289027+GermanFrash@users.noreply.github.com>
2024-04-14 14:40:13 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,12 +13,7 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.configure()
 
     def configure(self):
-         self.pushButton.clicked.connect(self.change_lable_after_clc)
-
-    def change_lable_after_clc(self):
-        self.label.setText('1')
-        self.label_2.setText('2')
-
+         pass
 
 
 if __name__ == "__main__":
