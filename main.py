f6aebeffffbd35bfe1ab7af07731adc68f6c4344
GermanFrash <166289027+GermanFrash@users.noreply.github.com>
2024-04-21 14:55:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 
-from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets
-
 from main_win import Ui_MainWindow
 
 
@@ -13,7 +11,63 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.configure()
 
     def configure(self):
-         pass
+        self.btn_clear.clicked.connect(
+            lambda: self.lineResult.clear()
+        )
+
+        self.btn_result.clicked.connect(self.__handle_equal)
+
+        for btn, val in self.__map_btn_values():
+            btn.clicked.connect(self.__handle(val))
+
+    def __handle(self, val: str):
+        def wrap():
+            math_op = ["*", "-", "+", "/"]
+            new_text = self.lineResult.text()
+
+            if new_text and (new_text[-1] in math_op) and (val in math_op):
+                return
+
+            new_text = new_text + val
+
+            new_text = new_text.replace("++", "+")
+            new_text = new_text.replace("--", "-")
+            new_text = new_text.replace("**", "*")
+            new_text = new_text.replace("//", "/")
+
+            self.lineResult.setText(new_text)
+
+        return wrap
+
+    def __handle_equal(self):
+        try:
+            result = float(eval(self.lineResult.text()))
+        except:
+            result = self.lineResult.text()
+            msg = (QtWidgets.QMessageBox(self))
+            msg.setText('Ошибка при вычеслениях')
+            msg.exec_()
+
+        self.lineResult.setText(str(result))
+
+    def __map_btn_values(self):
+        return [
+            (self.btn_num0, "0"),
+            (self.btn_num1, "1"),
+            (self.btn_num2, "2"),
+            (self.btn_num3, "3"),
+            (self.btn_num4, "4"),
+            (self.btn_num5, "5"),
+            (self.btn_num6, "6"),
+            (self.btn_num7, "7"),
+            (self.btn_num8, "8"),
+            (self.btn_num9, "9"),
+
+            (self.btn_plus, "+"),
+            (self.btn_minus, "-"),
+            (self.btn_mult, "*"),
+            (self.btn_div, "/"),
+        ]
 
 
 if __name__ == "__main__":
